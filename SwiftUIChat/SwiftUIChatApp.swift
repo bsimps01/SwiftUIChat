e6df7752562efcd08e69218004e0149b6c0d11b6
bsimps01 <b.albertsimpson@gmail.com>
2022-04-07 17:28:06 -0700

    added project

---
+++ SwiftUIChat/SwiftUIChatApp.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftUIChatApp.swift
+//  SwiftUIChat
+//
+//  Created by Benjamin Simpson on 3/23/22.
+//
+
+import SwiftUI
+import StreamChat
+import StreamChatSwiftUI
+
+@main
+struct SwiftUIChatApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
